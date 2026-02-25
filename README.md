4d21cfb25d43b59f8e11f7bfb59809e9e67cd4fd
rohi-del <rohitborannavar7@gmail.com>
2026-02-25 18:01:53 +0530

    modified

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# python
+# deploy-python-using-githubactions
---
+++ app.py
@@ -0,0 +1,9 @@
+with open("index.html", "w") as f:
+    f.write("""
+    <html>
+        <head><title>CI/CD Deployment</title></head>
+        <body>
+            <h1>Welcome, to CI/CD pipeline deployed on GitHub Actions Using Python</h1>
+        </body>
+    </html>
+    """)
