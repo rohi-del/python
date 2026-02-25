86c9bf449f0d48f763b6e5f286b1f7984d567ce5
rohi-del <rohitborannavar7@gmail.com>
2026-02-25 18:11:06 +0530

    modified

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ with open("index.html", "w") as f:
     <html>
         <head><title>CI/CD Deployment</title></head>
         <body>
-            <h1>Welcome, to CI/CD pipeline deployed on GitHub Actions Using Python</h1>
+            <h1>Welcome, hgiii CI/CD pipeline deployed on GitHub Actions Using Python</h1>
         </body>
     </html>
     """)
